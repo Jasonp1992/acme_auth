fd140990f7b06e8830277d91fd34ce069a314555
Robert West <robert.david.west@gmail.com>
2022-12-12 12:30:54 -0500

    Finish bcrypt tab

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const SALT_COUNT = 5;
 const SECRET_WORD = process.env.JWT;
 
 const config = {
@@ -38,19 +40,23 @@ User.byToken = async (token) => {
 };
 
 User.authenticate = async ({ username, password }) => {
-  const user = await User.findOne({
-    where: {
-      username,
-      password,
-    },
-  });
-  if (user) {
+  const user = await User.findOne({ where: { username } });
+  const isValid = bcrypt.compare(password, user.password);
+  if (isValid) {
     return jwt.sign({ id: user.id }, SECRET_WORD);
   }
   const error = Error("bad credentials");
   error.status = 401;
   throw error;
 };
+
+const hashPassword = async (password) => {
+  return await bcrypt.hash(password, SALT_COUNT);
+};
+
+User.beforeCreate(async (credential) => {
+  credential.password = await hashPassword(credential.password);
+});
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
@@ -59,8 +65,11 @@ const syncAndSeed = async () => {
     { username: "moe", password: "moe_pw" },
     { username: "larry", password: "larry_pw" },
   ];
+
   const [lucy, moe, larry] = await Promise.all(
-    credentials.map((credential) => User.create(credential))
+    credentials.map(async (credential) => {
+      User.create(credential);
+    })
   );
   return {
     users: {
